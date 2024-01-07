e778e7cf2ab14c5dcfd6ad8b97d5a636ba6c679b
jazmingar <mjazgs@gmail.com>
2024-01-07 16:13:41 -0600

    Template with footer

--- api/main.py
+++ api/main.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
 from fastapi import FastAPI
 #for templating 
 from fastapi import Request
@@ -12,10 +12,33 @@ __API_NAME__ = "TLOM API"
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="./templates")
+
+languages = ["lang1","lang2", "lang3"]
+#data requirements
+class Translate(BaseModel):
+    text: str 
+    source_lang: str 
+    target_lang: str 
+    #validate petition languages pair
+    @validator('source_lang','target_lang')
+    def valid_lang(cls, lang):
+        if lang not in languages:
+            raise ValueError("Lenguaje no disponible")
+        return lang
  
-@app.get('/')
+@app.get('/',  response_class=HTMLResponse)
 def home(request: Request):
         """Renders main page"""
-        return templates.TemplateResponse("home.html", 
-                {'request':request})
+        context = {'request': request, 'languages': languages}
+        return templates.TemplateResponse("home.html", context)
+
+@app.post('/translate')
+def json_analisis(args: Translate):
+        # Desempacar
+        #texto1=args.text
+        # Analisis
+        respuesta="adios"
+        # Empaquetar respuesta
+        return {"result":str(respuesta)}
+
 
