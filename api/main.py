695cc6a61f3e5e7b700bbc7a0454cfaddb5c59f7
jazmingar <mjazgs@gmail.com>
2024-02-26 11:37:10 -0600

    /translate with list index, /words first commit (almost working)

--- api/main.py
+++ api/main.py
@@ -46,6 +46,10 @@ def home(request: Request):
 
 @app.post('/translate')  
 def do_translation(t: Translate):
+    #store translation request in translation list
+    translations.append(t)
+    #index/ of the added translation req
+    t_id = len(translations)-1
     #for find model
     src_lang = t.source_lang
     models = {
@@ -61,13 +65,45 @@ def do_translation(t: Translate):
     
     #from the corresponding model file run translation function     
     translation=eval(model).translation_result(t)
-    translations.append(translation)
-    return translation 
+    #add translation result to translations list
+    my_trans=translations[t_id].dict()
+    #my_trans['translation'] = translation
+    my_trans.update({"translation": translation})
+    translations[t_id] = my_trans
+    return {'translation_id':t_id}  
     
-@app.get("/results")
-def get_results():
-    return {"translation": 'traducción'}
+
+@app.get("/words")
+def find_quote(t_id:int):
+    #get quote to split
+    quote = translations[t_id]['text']
+    #lista de palabras de la quote
+    #quote = 'piity tuunëëk kyukoxëëy poop'
+    my_words = quote.split()
+    print(my_words)
+       
+    with open('data/corpus.mir', 'r') as wixcrp:
+        for word in my_words:
+            flag = False
+            for linea in wixcrp:
+                linea = linea.rstrip()
+                if linea.find(word) == -1: continue
+                flag = True
+                print(linea)
+                    ##just print the first result:
+                    ##break
+                ##else: print('No se encontró ningún ejemplo de uso')
+            if flag==False:
+                print('No matched')
+            
+    
+    return 'hey'
 
 @app.get("/history")
 def get_history():
     return translations
+
+
+@app.get("/results")
+def get_results():
+    return {"translation": 'traducción por añadir'}
--- models/model1.py
+++ models/model1.py
@@ -4,4 +4,4 @@ def translation_result(t):
     source_lang=t.source_lang
     target_lang=t.target_lang
     result = f'Traducción del {source_lang} al {target_lang} del texto: {text}'
-    return {'translation': result}
+    return str(result)
