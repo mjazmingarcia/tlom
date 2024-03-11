ec9a289680f02dd7ee599fa9ba98df0cb3de8712
jazmingar <mjazgs@gmail.com>
2024-03-11 16:48:10 -0600

    working with Flores model (first commit)

--- models/model2.py
+++ models/model2.py
@@ -2,8 +2,9 @@ from transformers import M2M100ForConditionalGeneration, M2M100Tokenizer
 
 
 def generate_transformer(t):
-    hi_text = "जीवन एक चॉकलेट बॉक्स की तरह है।"
-    chinese_text = "生活就像一盒巧克力。"
+    src_lang = t.source_lang
+    my_text = t.text
+    tgt_lang = t.target_lang
 
     model = M2M100ForConditionalGeneration.from_pretrained("seyoungsong/flores101_mm100_175M")
     tokenizer: M2M100Tokenizer = M2M100Tokenizer.from_pretrained("seyoungsong/flores101_mm100_175M")
@@ -12,11 +13,12 @@ def generate_transformer(t):
     tokenizer.lang_code_to_token = {s.strip("_"): s for s in tokenizer.lang_token_to_id}
     tokenizer.lang_code_to_id = {s.strip("_"): i for s, i in tokenizer.lang_token_to_id.items()}
     tokenizer.id_to_lang_token = {i: s for s, i in tokenizer.lang_token_to_id.items()}
-    # translate Chinese to English
-    tokenizer.src_lang = "zh"
-    encoded_zh = tokenizer(chinese_text, return_tensors="pt")
-    generated_tokens = model.generate(**encoded_zh, forced_bos_token_id=tokenizer.get_lang_id("en"))
+    # translate from src_lang to tgt_lang
+    tokenizer.src_lang = src_lang
+    my_encoded = tokenizer(my_text, return_tensors="pt")
+    generated_tokens = model.generate(**my_encoded, forced_bos_token_id=tokenizer.get_lang_id(tgt_lang))
     resultado=tokenizer.batch_decode(generated_tokens, skip_special_tokens=True)
+    
     return str(resultado)
 # => "Life is like a chocolate box."
 
